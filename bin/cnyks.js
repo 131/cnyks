00ca6caa6b8a8b6ded4800449d124d969b2c91df
Mejdi Ourir <m.ourir@ivsweb.com>
2015-10-12 15:40:35 +0200

    Typo..

--- bin/cnyks.js
+++ bin/cnyks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var Cnyks     = require('../lib');
-var parseargs = require('nyks/process/parseargs');
+var parseargs = require('nyks/process/parseArgs');
 
 var cmdline        = process.argv.slice(2);
 var cmdline_parsed = parseargs(cmdline);
