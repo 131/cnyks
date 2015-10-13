92ae021e37255abb9cb230c38df29a7c9e1bbff8
François Leurent <131.php@cloudyks.org>
2015-10-14 01:30:07 +0200

    Show metadata on non command

--- bin/cnyks.js
+++ bin/cnyks.js
@@ -2,9 +2,16 @@
 
 var Cnyks     = require('../lib');
 var parseargs = require('nyks/process/parseArgs');
+var box       = require('nyks/cli/box');
 
 var cmdline        = process.argv.slice(2);
 var cmdline_parsed = parseargs(cmdline);
 
+if(!cmdline.length) {
+  var man = require('../package.json');
+  return box("cnyks", JSON.stringify({
+    version : man.version, path : __dirname
+  }, null, 2) );
+}
 
 Cnyks.start(cmdline_parsed.args[0], cmdline_parsed.dict);
