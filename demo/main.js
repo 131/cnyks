ac40185333beba05cac044fe40182cdaaa0e823a
François Leurent <fleurent@ivsweb.com>
2016-10-13 15:28:16 +0200

    WIp demo resources

--- demo/main.js
+++ demo/main.js
@@ -5,25 +5,46 @@ const Terminal     = require('xterm2');
   require('xterm2/addons/fit')(Terminal);
   require('xterm2/addons/readline')(Terminal);
 
+
 const Foo   = require('./foo');
 const cnyks = require('../');
 const promisify = require('nyks/function/promisify');
 
 const term = new Terminal();
   window.term = term; //usefull for debug
+var $ = document.querySelector.bind(document);
 
-createTerminal(document.querySelector('#terminal-container'));
 
-function createTerminal(terminalContainer) {
-  term.open(terminalContainer);
-  term.fit();
+$("#runner").addEventListener('click', function(){
+  var module = {};
+    Function("module", $("#klass").value).call(null, module);
 
-  var runner = cnyks.start(Foo, {
 
+  var runner = cnyks.start(module.exports, {
     'ir://name'  : 'foo',
     'ir://prompt' : promisify(term.readline, term),
     'ir://stderr' : term.write.bind(term),
     'ir://stdout' : term.write.bind(term),
   });
+
+  box.setCols( term.cols - 1 ); //
+  runner
+});
+
+createTerminal($('#terminal-container'));
+
+window.onresize =  function() {
+console.log("Resizing");
+  term.fit();
+  term.reset();
+
+  runner.setCols(term.cols- 1);
+  runner.run(); //previous run has been aborted by term.reset
+}
+
+
+function createTerminal(terminalContainer) {
+  term.open(terminalContainer);
+  term.fit();
 }
 
