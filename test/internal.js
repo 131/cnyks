763da1a353f05c3b7d3933f20e13a4da81025459
François Leurent <fleurent@ivsweb.com>
2016-11-06 16:45:37 +0100

    Bump istanbul , increase code coverage

--- test/internal.js
+++ test/internal.js
@@ -23,11 +23,35 @@ describe("Internal lookup", function(){
     process.stdin.unref();
  });
 
+  it("Should also scan instances", function() {
+    var fuu = require("./data/fuu.js");
+    var child = cnyks.start(new fuu());
+    expect(child.command_alias("runner", "quit", "qq")).to.be(undefined);
+    expect(child.command_alias("runner", "quita", "qq")).to.be(false);
+    process.stdin.unref();
+ });
 
-  it("Should execute command provided on start", function() {
 
-    var child = cnyks.start(require("./data/fuu.js"), {"ir://json": true, "ir://start" : "sum", "a" : 3, "b" : 5 });
+  it("Should execute command provided on start", function(done) {
 
+    var collect = function(what){
+      collect[what] = '';
+      return function(msg){
+        collect[what] += msg;
+      }
+    }
+    var child = cnyks.start(require("./data/fuu.js"), {
+      "ir://json": true, "ir://start" : "sum", "a" : 3, "b" : 5,
+
+      "ir://stderr" : collect("stderr"),
+      "ir://stdout" : collect("stdout"),
+      "ir://prompt" : function(){ return Promise.resolve("quit\n") ;},
+    });
+
+    setImmediate(function(){
+      expect(collect.stdout).to.eql(8);
+      done();
+    });
  });
 
 
