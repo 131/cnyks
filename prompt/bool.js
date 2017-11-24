7e36d5a3dcdbfad3afb6fc7dddedf1aa61a60fce
François Leurent <fleurent@ivsweb.com>
2017-11-24 10:47:28 +0100

    Switch prompt to es7

--- prompt/bool.js
+++ prompt/bool.js
@@ -5,12 +5,12 @@ const promisify = require('nyks/function/promisify');
 const prompt   = promisify(read);
 const toBool = require('nyks/lang/toBool');
 
-const bool_prompt  = function*(msg, base){
-  var response = (yield prompt({prompt:  msg + (base ? "[Y/n]" : "[y/N]")})).toLowerCase();
+const bool_prompt  = async function(msg, base) {
+  var response = (await prompt({prompt:  msg + (base ? "[Y/n]" : "[y/N]")})).toLowerCase();
 
   if(response && ["yes", "y", "no", "n"].indexOf(response) == -1) {
     console.error("Please type [yes] or [no]");
-    return yield bool_prompt(msg, base);
+    return await bool_prompt(msg, base);
   }
 
   response = response ? toBool(response) : base;
