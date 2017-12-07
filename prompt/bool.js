e95209318ea02949f0346add5289a99664c23a0c
François Leurent <fleurent@ivsweb.com>
2017-12-07 09:57:15 +0100

    Use promise for prompt/bool

--- prompt/bool.js
+++ prompt/bool.js
@@ -5,16 +5,18 @@ const promisify = require('nyks/function/promisify');
 const prompt   = promisify(read);
 const toBool = require('nyks/lang/toBool');
 
-const bool_prompt  = async function(msg, base) {
-  var response = (await prompt({prompt:  msg + (base ? "[Y/n]" : "[y/N]")})).toLowerCase();
+const bool_prompt  = function(msg, base) {
 
-  if(response && ["yes", "y", "no", "n"].indexOf(response) == -1) {
-    console.error("Please type [yes] or [no]");
-    return await bool_prompt(msg, base);
-  }
+  return prompt({prompt:  msg + (base ? "[Y/n]" : "[y/N]")}).then( (response) => {
+    response = response.toLowerCase();
 
-  response = response ? toBool(response) : base;
-  return response;
+    if(response && ["yes", "y", "no", "n"].indexOf(response) == -1) {
+      console.error("Please type [yes] or [no]");
+      return bool_prompt(msg, base);
+    }
+    response = response ? toBool(response) : base;
+    return response;
+  });
 }
 
 module.exports = bool_prompt;
