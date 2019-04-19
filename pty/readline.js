760d172018236551b14210c9a756963dd66fa092
François Leurent <131.js@cloudyks.org>
2019-04-19 14:27:13 +0200

    With readline & stream mode

---
+++ pty/readline.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const defer = require('nyks/promise/defer');
+
+const readline = async function(opt) {
+
+  let defered = defer();
+  let line = "";
+
+  let draw = () => {
+    //this.write('\x1b[2K'); // erase all line
+    this.write(`\r${opt.prompt}${line}`);
+    this.write('\x1b[K'); //erase right
+  };
+
+  draw();
+
+  function read(buf) {
+    if(buf == "\x0d") { //enter
+      this.removeListener("data", read);
+      defered.resolve(line);
+      this.write("\r\n");
+      return;
+    }
+    if(buf == "\x03") { //cancel
+      this.removeListener("data", read);
+      defered.reject(Error("canceled"));
+      return;
+    }
+
+    if(buf == "\x7f") //backslash
+      line = line.substr(0, line.length - 1);
+
+    if(/^[ -~]+$/.test(buf)) //printable
+      line = line + buf;
+
+    draw();
+  }
+
+  this.on("data", read);
+  return defered;
+};
+
+
+module.exports = readline;
