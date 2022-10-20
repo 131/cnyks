f0ad93b767f86855e0fbd8e113a2ac522ae581d7
Francois Leurent <131.code@leurent.email>
2022-10-20 12:12:15 +0200

    Fix --ir://raw algo

--- test/04-mirror.js
+++ test/04-mirror.js
@@ -17,14 +17,13 @@ describe("Test mirrors", function() {
     var payload = await drain(child.stdout);
     expect(Buffer.isBuffer(payload)).to.be.ok();
     expect(payload.toString("utf-8").trim()).to.eql("42");
-
   });
 
   it("should test mirror (json)", async function() {
     var args = [];
 
     var foo = {"this" : "is", "a" : ["complex", null, 45, "object"]};
-    args.push("bin/cnyks.js", "./test/data/fuu.js", "--ir://raw", "--ir://run=mirror", "--foo::json=" + JSON.stringify(foo));
+    args.push("bin/cnyks.js", "./test/data/fuu.js", "--ir://json", "--ir://run=mirror", "--foo::json=" + JSON.stringify(foo));
     var child = cp.spawn(process.execPath, args);
     var payload = await drain(child.stdout);
     expect(JSON.parse(payload.toString("utf-8"))).to.eql(foo);
