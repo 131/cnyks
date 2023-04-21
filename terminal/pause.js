ce5d3e3143fafa86b88808fdb1d92ffcb51d44bc
Francois Leurent <131.code@leurent.email>
2023-04-21 10:30:28 +0200

    Revert "Bump to read@2"
    This reverts commit 851dab837894ad4f82fb50ed88ca2ad0f05b08f4.

--- terminal/pause.js
+++ terminal/pause.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const read     = require('read');
+const read     = require('read-merge-28');
+const promisify = require('nyks/function/promisify');
+const prompt   = promisify(read);
 
 const pause  = function(msg) {
   if(msg) console.error(msg);
