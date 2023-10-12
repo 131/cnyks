b8f9f1bd19321c531af594fc4296f1e3e527530b
Francois Leurent <131.code@leurent.email>
2023-10-12 16:58:01 +0200

    Fix typo in terminal/pause

--- terminal/pause.js
+++ terminal/pause.js
@@ -4,7 +4,7 @@ const read     = require('read');
 
 const pause  = function(msg) {
   if(msg) console.error(msg);
-  return prompt({prompt : `Press [Enter] to continue.`, silent :true});
+  return read({prompt : `Press [Enter] to continue.`, silent : true});
 };
 
 module.exports = pause;
